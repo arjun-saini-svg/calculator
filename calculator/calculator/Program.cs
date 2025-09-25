87b6da1e143f1ec94590493aa12186fbdc029096
arjun-saini-svg <asphcasee@gmail.com>
2025-09-25 22:31:16 +0530

    basic structure

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calculator Program");
+        Console.WriteLine("------------------");
+
+        Console.Write("Enter number 1: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter number 2: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter an option:");
+        
+    }
+}
